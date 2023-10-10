1a59797b66e44b183e2c6fd33eca4de3f6f90ff3
jagiraldorestrepo <ja.giraldorestrepo@gmail.com>
2023-10-10 08:14:22 -0500

    Update Alfabeto.java

--- src/main/java/comunicacion/Alfabeto.java
+++ src/main/java/comunicacion/Alfabeto.java
@@ -3,4 +3,43 @@ package comunicacion;
 public class Alfabeto extends Pictograma{
 	private String[] letras;
 	private String interpretacion;
+	
+	public Alfabeto(String origen, String[] letras, String interpretacion) {
+		super(origen);
+		this.letras = letras;
+		this.interpretacion = interpretacion;
+	}
+	
+	public int cantidadLetras() {
+		return letras.length;
+	}
+	public String interpretacion() {
+		return interpretacion;
+	}
+	public String toString() {
+		String alf="";
+		for(int i=0;i<letras.length-1; i++)
+		{
+		    alf+=letras[i]+", ";
+	    }
+		alf+=letras[letras.length-1];
+		return alf;
+	}
+
+	public String[] getLetras() {
+		return letras;
+	}
+
+	public void setLetras(String[] letras) {
+		this.letras = letras;
+	}
+
+	public String getInterpretacion() {
+		return interpretacion;
+	}
+
+	public void setInterpretacion(String interpretacion) {
+		this.interpretacion = interpretacion;
+	}
+	
 }
