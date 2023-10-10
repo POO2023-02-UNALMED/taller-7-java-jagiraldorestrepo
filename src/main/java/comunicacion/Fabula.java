7706fe7f285263c7fe90aa04a0e58202b1319146
jagiraldorestrepo <ja.giraldorestrepo@gmail.com>
2023-10-10 08:15:00 -0500

    Update Fabula.java

--- src/main/java/comunicacion/Fabula.java
+++ src/main/java/comunicacion/Fabula.java
@@ -3,4 +3,41 @@ package comunicacion;
 public class Fabula extends Escrito{
 	private String ensenanza;
 	private String interpretacion;
+	
+	
+	public Fabula(String origen, String titulo, String autor, int paginas, String ensenanza, String interpretacion) {
+		super(origen, titulo, autor, paginas);
+		this.ensenanza = ensenanza;
+		this.interpretacion = interpretacion;
+	}
+	
+	public String getEnsenanza() {
+		return ensenanza;
+	}
+	public void setEnsenanza(String ensenanza) {
+		this.ensenanza = ensenanza;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
+
+
+
+
