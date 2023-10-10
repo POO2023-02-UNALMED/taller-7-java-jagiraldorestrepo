59828ca19fa369ab8f974b313211a1860e2ee059
jagiraldorestrepo <ja.giraldorestrepo@gmail.com>
2023-10-10 08:15:52 -0500

    Update Libro.java

--- src/main/java/comunicacion/Libro.java
+++ src/main/java/comunicacion/Libro.java
@@ -5,4 +5,34 @@ public class Libro extends Escrito{
 	private String editorial;
 	private String edicion;
 	private String interpretacion;
+	
+	
+	public Libro(String origen, String titulo, String autor, int paginas, String co_autor, String editorial,String edicion, String interpretacion) {
+		super(origen, titulo, autor, paginas);
+		this.co_autor = co_autor;
+		this.editorial = editorial;
+		this.edicion = edicion;
+		this.interpretacion = interpretacion;
+	}
+
+	public String getCo_autor() {
+		return co_autor;
+	}
+
+	public void setCo_autor(String co_autor) {
+		this.co_autor = co_autor;
+	}
+
+	public String getEditorial() {
+		return editorial;
+	}
+
+	public void setEditorial(String editorial) {
+		this.editorial = editorial;
+	}
+
+	public String getEdicion() {
+		return edicion;
+	}
+
 }
