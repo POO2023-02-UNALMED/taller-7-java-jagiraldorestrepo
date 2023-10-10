89cd9a74b778cf39c884f1ccbc345604b08f6591
jagiraldorestrepo <ja.giraldorestrepo@gmail.com>
2023-10-10 08:17:47 -0500

    Update Tesis.java

--- src/main/java/comunicacion/Tesis.java
+++ src/main/java/comunicacion/Tesis.java
@@ -6,4 +6,32 @@ public class Tesis extends Escrito {
 	private String conclusion;
 	private String referencias;
 	private String interpretacion;
+	
+	
+	public Tesis(String origen, String titulo, String autor, int paginas, String idea, String[] argumentos,
+			String conclusion, String referencias, String interpretacion) {
+		super(origen, titulo, autor, paginas);
+		this.idea = idea;
+		this.argumentos = argumentos;
+		this.conclusion = conclusion;
+		this.referencias = referencias;
+		this.interpretacion = interpretacion;
+	}
+
+	public String getIdea() {
+		return idea;
+	}
+
+	public void setIdea(String idea) {
+		this.idea = idea;
+	}
+
+	public String[] getArgumentos() {
+		return argumentos;
+	}
+
+	public void setArgumentos(String[] argumentos) {
+		this.argumentos = argumentos;
+	}
+
 }
