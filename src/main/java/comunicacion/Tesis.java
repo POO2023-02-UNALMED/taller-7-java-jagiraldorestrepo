a4a7dccc9b944b34fcebc2f78264a56bfabf2a95
jagiraldorestrepo <ja.giraldorestrepo@gmail.com>
2023-10-10 08:13:44 -0500

    Update Tesis.java

--- src/main/java/comunicacion/Tesis.java
+++ src/main/java/comunicacion/Tesis.java
@@ -1,5 +1,9 @@
 package comunicacion;
 
-public class Tesis {
-
+public class Tesis extends Escrito {
+	private String idea;
+	private String[] argumentos;
+	private String conclusion;
+	private String referencias;
+	private String interpretacion;
 }
